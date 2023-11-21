f5c9aba37399afaeda8bac346c234541aa9dd3aa
哒哒哒 <17779916+malegedandan@users.noreply.github.com>
2023-11-21 11:54:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,10 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/Cross-Kit/xrayKit/releases/download/0.1.61/XrayKit.xcframework.zip",
-      checksum: "c27a231e665856afa9fc9baf229edc1478c7902ae4fa8e76910deee715d4de0d"
+      // url: "https://github.com/Cross-Kit/xrayKit/releases/download/0.1.61/XrayKit.xcframework.zip",
+      // checksum: "c27a231e665856afa9fc9baf229edc1478c7902ae4fa8e76910deee715d4de0d"
+      url: "https://github.com/xby023/XrayKit/archive/refs/tags/0.1.45.zip",
+      checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
 )
